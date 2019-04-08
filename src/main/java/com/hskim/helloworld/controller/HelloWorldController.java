4476ebf2e2dbb87fe0c6e26b6930a28a2ae3bf78
hskim <whitekim88@gmail.com>
2019-04-09 08:57:50 +0900

    로그 남기기

--- src/main/java/com/hskim/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/hskim/helloworld/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.hskim.helloworld.controller;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +12,9 @@ public class HelloWorldController {
   
   @GetMapping("/")
   public String index() {
+    LocalDate currentDate = LocalDate.now();
+    LocalTime currentTime = LocalTime.now();
+    System.out.println("===================== " + currentDate + " " + currentTime + " =====================");
     return "Hello World";
   }
 }
